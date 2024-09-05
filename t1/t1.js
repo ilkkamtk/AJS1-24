c0e276b5bb7f8e63ddbba6e847361f11fc78f6d3
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-09-05 09:51:53 +0300

    virheet

--- t1/t1.js
+++ t1/t1.js
@@ -11,6 +11,9 @@ const sodexoBTN = document.querySelector('#sodexo');
 const compassBTN = document.querySelector('#compass');
 const resetBTN = document.querySelector('#reset');
 
+// kaikki ravintolat tänne
+let raflat = [];
+
 closeModal.addEventListener('click', () => {
   modaali.close();
 });
@@ -20,54 +23,72 @@ const haeRavintolat = async () => {
 };
 
 const teeRavintolaLista = async (restaurants) => {
-  kohde.innerHTML = '';
-  // filteröinti *************************
+  try {
+    // testaa globaali raflat
+    console.log(raflat);
 
-  sodexoBTN.addEventListener('click', () => {
-    const filteredRestaurants = restaurants.filter(
-      (restaurant) => restaurant.company === 'Sodexo'
-    );
-    teeRavintolaLista(filteredRestaurants);
-  });
+    kohde.innerHTML = '';
+    // filteröinti *************************
 
-  // *************************************
+    sodexoBTN.addEventListener('click', () => {
+      const filteredRestaurants = restaurants.filter(
+        (restaurant) => restaurant.company === 'Sodexo'
+      );
+      teeRavintolaLista(filteredRestaurants);
+    });
 
-  restaurants.sort((a, b) => a.name.localeCompare(b.name));
+    // *************************************
 
-  restaurants.forEach((restaurant) => {
-    if (restaurant) {
-      const {_id} = restaurant;
+    restaurants.sort((a, b) => a.name.localeCompare(b.name));
 
-      // ravintolan HTML rivi
-      const rivi = restaurantRow(restaurant);
+    restaurants.forEach((restaurant) => {
+      if (restaurant) {
+        const {_id} = restaurant;
 
-      rivi.addEventListener('click', async () => {
-        modaali.showModal();
-        info.innerHTML = '<div>Ladataa...</div>';
+        // ravintolan HTML rivi
+        const rivi = restaurantRow(restaurant);
 
-        const korostetut = document.querySelectorAll('.highlight');
-        korostetut.forEach((korostettu) => {
-          korostettu.classList.remove('highlight');
+        rivi.addEventListener('click', async () => {
+          try {
+            modaali.showModal();
+            info.innerHTML = '<div>Ladataa...</div>';
+
+            const korostetut = document.querySelectorAll('.highlight');
+            korostetut.forEach((korostettu) => {
+              korostettu.classList.remove('highlight');
+            });
+
+            rivi.classList.add('highlight');
+
+            // hae päivän ruokalista
+            const paivanLista = await fetchData(
+              apiURL + `/api/v1/restaurants/daily/${_id}/fi`
+            );
+
+            console.log('päivan lista', paivanLista.courses);
+            // tulosta päivän ruokalista
+            const ravintolaHTML = restaurantModal(
+              restaurant,
+              paivanLista.courses
+            );
+            info.innerHTML = '';
+            info.insertAdjacentHTML('beforeend', ravintolaHTML);
+          } catch (error) {
+            console.log(error);
+          }
         });
 
-        rivi.classList.add('highlight');
-
-        // hae päivän ruokalista
-        const paivanLista = await fetchData(
-          apiURL + `/api/v1/restaurants/daily/${_id}/fi`
-        );
-
-        console.log('päivan lista', paivanLista.courses);
-        // tulosta päivän ruokalista
-        const ravintolaHTML = restaurantModal(restaurant, paivanLista.courses);
-        info.innerHTML = '';
-        info.insertAdjacentHTML('beforeend', ravintolaHTML);
-      });
-
-      kohde.append(rivi);
-    }
-  });
+        kohde.append(rivi);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const raflat = await haeRavintolat();
-teeRavintolaLista(raflat);
+try {
+  raflat = await haeRavintolat();
+  teeRavintolaLista(raflat);
+} catch (error) {
+  console.log(error);
+}
